46acb3b2ace54abe6cc1319edf0e85cfbc2280d1
rachele.bellini <rachele.bellini@pixomondo.com>
2016-07-21 10:47:37 -0700

    src files: added main for command line and refactored other modules

---
+++ quilt/main_cmd.py
@@ -0,0 +1,234 @@
+#! /usr/bin/env python
+"""
+Quilt command line interface
+"""
+
+# import builtin modules
+from __future__ import division
+import time
+import sys
+import os
+from os.path import split, splitext, join, isdir, isfile
+from pprint import pprint
+import glob
+from multiprocessing import cpu_count
+sys.path.insert(0, r"C:\dev\textures\Lib\site-packages")
+
+# import 3rd party modules
+from PIL import Image
+import click
+from click import BadParameter
+
+# import internal modules
+from process import Quilt
+from utils import save, show, imresize, img2matrix
+
+
+def set_src_path(ctx, _, path):
+    """
+    Solves source path: looks for all the matching files. If nothing is found,
+    errors out
+    Args:
+        ctx (click.Context): the current context
+        _: required by click callbacks but ignored
+        path: source path/s
+
+    Returns:
+        list or string instance of the resolved path/s
+    """
+    path = os.path.abspath(path)
+    paths = sorted(glob.glob(path))
+    if not paths:
+        raise click.BadParameter('Source file/s could not be found',
+                                 param_hint='source')
+    return paths
+
+
+@click.command(
+    name='quilt',
+    context_settings={'help_option_names': ['-h', '--help']})
+@click.argument(
+    'src',
+    type=click.Path(),
+    callback=set_src_path)
+@click.option(
+    '-imask', '--input_mask',
+    type=click.Path(exists=True, dir_okay=False),
+    callback=lambda ctx, _, x: Image.open(x) if x else None,
+    help="Mask on the input to remove image areas with unwanted content")
+@click.option(
+    '-iscale', '--input_scale',
+    type=click.FLOAT,
+    default=1,
+    help="Scale to apply to the input image before computing")
+@click.option(
+    '-dst', '--destination',
+    type=click.Path(),
+    help='Path to the result folder')
+@click.option(
+    '-owidth', '--output_width',
+    type=click.INT,
+    help='Width of the result image')
+@click.option(
+    '-oheight', '--output_height',
+    type=click.INT,
+    help='Height of the result image')
+@click.option(
+    '-cmask', '--cut_mask',
+    type=click.Path(exists=True, dir_okay=False),
+    callback=lambda ctx, _, x: img2matrix(Image.open(x)) if x else None,
+    help='Mask for the output image')
+@click.option(
+    '-tile', '--tilesize',
+    type=click.INT,
+    default=30,
+    help='Size of the small tiles')
+@click.option(
+    '-over', '--overlap',
+    type=click.INT,
+    default=10,
+    help='Size of the small overlap')
+@click.option(
+    '-btile', '--big_tilesize',
+    type=click.INT,
+    default=500,
+    help='Size of the big tiles')
+@click.option(
+    '-bover', '--big_overlap',
+    type=click.INT,
+    default=100,
+    help='Size of the big overlap')
+@click.option(
+    '-err', '--error',
+    type=click.FLOAT,
+    default=0.2,
+    help='Error accepted in selecting similar tiles')
+@click.option(
+    '--constraint_start',
+    type=click.BOOL,
+    default=False,
+    help='The first tile in dst is equal the one in src')
+@click.option(
+    '--cores',
+    type=click.INT,
+    default=cpu_count()-2,
+    help='Number of available cores')
+@click.option(
+    '-multiproc', '--multiprocess',
+    type=click.BOOL,
+    default=True,
+    help='Multiprocess (faster) or single process computation')
+@click.option(
+    '-rot', '--rotations',
+    type=click.IntRange(0, 4),
+    default=0,
+    help='Number of 90 degrees rotations to apply to src')
+@click.option(
+    '--debug/--no-debug',
+    default=False,
+    help='Enable/Disable debug messages and image display')
+def cli(src, **kwargs):
+
+    pprint(src)
+    pprint(kwargs)
+
+    # turn paths into matrices. We need to do it here because we need iscale val
+    src_matrices = []
+    for p in src:
+        img = Image.open(p)
+        # resize
+        img = imresize(img, scale=kwargs['input_scale'])
+        src_matrices.append(img2matrix(img))
+    if kwargs['input_mask'] is not None:
+        kwargs['input_mask'] = imresize(kwargs['input_mask'],
+                                        scale=kwargs['input_scale'])
+        kwargs['input_mask'] = img2matrix(kwargs['input_mask'])
+
+    kwargs.pop('input_scale')
+    hgt = kwargs.pop('output_height') or src_matrices[0].shape[0]
+    wdt = kwargs.pop('output_width') or src_matrices[0].shape[1]
+    kwargs['output_size'] = [hgt, wdt]
+
+    # destination paths
+    dst = kwargs.pop('destination')
+    # for every case: create 'path_form' which contains: a form to derive the
+    # paths of the temporal result and, if necessary, the paths of the
+    # additional layers
+    if not dst:
+        prefix, ext = splitext(src[0])
+        path_form = '{1}_result{0}{2}'.format('{0}', prefix, ext)
+    else:
+        msg = 'Location of the destination file could not be found: {0}'
+        # file
+        if splitext(dst)[1]:
+            # check if the location exists
+            result_dir = split(dst)[0]
+            if not isdir(result_dir):
+                raise BadParameter(msg.format(result_dir), param_hint='dst')
+            # path form: contains the single path name and the path form in case
+            # there are multiple layers
+            path_form = '{1}{0}{2}'.format('{0}', *splitext(dst))
+
+        # directory
+        else:
+            if not isdir(dst):
+                raise BadParameter(msg.format(dst), param_hint='dst')
+            base_name = splitext(split(src[0])[1])[0]
+            path_form = join(dst, '{1}_result{0}.png'.format('{0}', base_name))
+
+    # master result
+    if len(src) == 1:
+        result_path = path_form.format('')
+    else:
+        # there are multiple layers: every output file has the suffix _layer
+        result_path = [path_form.format('_layer' + str(i))
+                       for i in xrange(len(src))]
+
+    # derive the temp path to pass to Quilt
+    kwargs['result_path'] = path_form[1].format('_temp')
+
+    launch_quilt(result_path, src_matrices, **kwargs)
+
+
+def launch_quilt(final_path, src, **kwargs):
+    print kwargs['debug']
+
+    multi_proc = kwargs.pop('multiprocess')
+    debug = kwargs.pop('debug')
+
+    # ------------- quilt --------------
+
+    qs = time.time()
+    print 'Quilt started at {0}'.format(time.strftime("%H:%M:%S"))
+
+    # compute quilting
+
+    Quilt.debug = debug
+    q = Quilt(src, **kwargs)
+    if multi_proc:
+        q.optimized_compute()
+    else:
+        q.compute()
+
+    # get the result
+    result = q.get_result()
+    if debug:
+        show(result)
+    save(result, final_path)
+
+    # track = None
+    # if config['preview']:
+    #     print '\n- PREVIEW -'
+    #     track = q.optimized_compute_big(preview=True)
+    #     track = q.optimized_compute_big(preview=True, tracked=track)
+    # if config['full']:
+    #     print '\n- FULL -'
+    #     track = q.optimized_compute_big(preview=False, tracked=track)
+
+    t = (time.time()-qs)/60
+    print 'Quilt took {0:0.6f} minutes'.format(t)
+    print 'End {0}'.format(time.strftime("%H:%M:%S"))
+
+
+if __name__ == '__main__':
+    cli()
---
+++ quilt/utils.py
@@ -0,0 +1,204 @@
+#! /usr/bin/env python
+"""
+Utility functions.
+"""
+
+# import builtin modules
+from copy import deepcopy
+
+# import 3rd party modules
+import numpy as np
+from numpy import multiply
+from PIL import Image
+
+
+def filter_img(img_a, img_b, mask):
+    """
+    Combines two RGB images according to the binary weight mask mask.
+    Where the mask is white the first image will be copied, where the mask is
+    black the second image will be copied.
+    The images and the mask must have the same dimensions.
+
+    Args:
+        img_a: first image
+        img_b: second image
+        mask: binary mask
+
+    Returns:
+        an image with pixels from img_a (where mask = 1), and pixels from img_b
+        (where mask = 0)
+    """
+    if not (
+       img_a.shape[0:2] == img_b.shape[0:2] and img_a.shape[0:2] == mask.shape):
+        raise ValueError('Matrices dimensions mismatch: got {0}, {1} and {2}'.
+                         format(img_a.shape, img_b.shape, mask.shape))
+    res = np.zeros(img_a.shape)
+    for i in range(0, 3):
+        res[:, :, i] = multiply(img_a[:, :, i], (mask == 1)) + \
+                       multiply(img_b[:, :, i], (mask == 0))
+    return res
+
+
+def imresize(img, size=[0, 0], height=None, width=None, scale=None):
+    """
+    Resize an image based on new height and width or a scale value
+    Args:
+        img: image (PIL.Image)
+        size: array [width, height] containing the new sizes
+        height: new desired height
+        width: new desired width
+        scale: scale value
+
+    Returns: resized image
+    """
+    # if it is a matrix: turn it into image first
+    is_matrix = isinstance(img, np.ndarray)
+    if is_matrix:
+        img = matrix2img(img)
+
+    if height and width:
+        size = [width, height]
+
+    # find the scale value if only on dimension is given
+    if height and not width:
+        scale = (height / float(img.shape[1]))
+    if width and not height:
+        scale = (width / float(img.shape[0]))
+
+    h = size[0] or int(float(img.size[1]) * float(scale))
+    w = size[1] or int(float(img.size[0]) * float(scale))
+    result = img.resize((w, h), Image.ANTIALIAS)
+
+    return img2matrix(result) if is_matrix else result
+
+
+def img2matrix(img):
+    """
+    Converts the input image into a matrix of float values in the range [0, 1].
+    """
+    matrix = np.asarray(img).astype(np.float)
+    matrix = im2double(matrix)
+    return matrix
+
+
+def matrix2img(matrix, adjust_values=True):
+    """
+    Converts an image into a matrix.
+    """
+    if adjust_values and np.max(matrix) <= 1:
+        matrix *= 255
+    matrix = np.uint8(matrix)
+    if len(matrix.shape) == 3 and matrix.shape[2] == 3:
+        return Image.fromarray(matrix, 'RGB')
+    return Image.fromarray(matrix)
+
+
+def gray2rgb(im):
+    """
+    Convert a gray scale image (1 channel) to RGB (3 channels)
+    Args:
+        im: image or matrix to be converted
+
+    Returns:
+        RGB image
+    """
+    if len(im.shape) == 3 and im.shape[2] == 3:
+        return im
+    if len(im.shape) == 2:
+        return np.asarray(np.dstack((im, im, im)))
+    raise ValueError('Input image must be 2 or 3 dimensional')
+
+
+def rgb2gray(im):
+    """
+    Convert a RGB image (3 channels) to a gray-scale image (1 channel)
+    Args:
+        im: image to convert
+
+    Returns:
+        gray-scale image
+    """
+    if len(im.shape) == 2:
+        return im
+    if len(im.shape) == 3 and im.shape[2] == 3:
+        r, g, b = im[:, :, 0], im[:, :, 1], im[:, :, 2]
+        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+        if im.dtype is np.dtype('uint8'):
+            gray = np.uint8(gray)
+        return gray
+    raise ValueError('Input mask must be 2 or 3 dimensional')
+
+
+def im2double(im):
+    """
+    Convert an uint8 image to float.
+    Args:
+        im: image to convert
+
+    Returns:
+        image with float values
+    """
+    if im.dtype == np.dtype('float64'):
+        if np.max(im) > 1:
+            return im / 255.0
+        return im
+    if im.dtype == np.dtype('uint8'):
+        return im.astype('float') / 255.0
+    raise ValueError("im2double: unsupported image type. Found: {0}".format(
+                             im.dtype))
+
+
+def show(img):
+    """
+    Display image. Can display multiple images.
+    Args:
+        img: image (or list of images) to display
+        title: title of the image/s
+    """
+    if not isinstance(img, list):
+        img = [img]
+    for i in img:
+        im = deepcopy(i)
+        if im.dtype == np.dtype('uint8'):
+            # clamp values to [0, 255]
+            im[im > 255] = 255
+            im[im < 0] = 0
+            matrix2img(im).show()
+        elif im.dtype == np.dtype('float64'):
+            # clamp values to [0, 1]
+            im[im > 1] = 1
+            im[im < 0] = 0
+            matrix2img(im).show()
+        else:
+            raise ValueError("show: unsupported image type: found {0}".format(
+                             im.dtype))
+
+
+def save(img, path):
+    """
+    Save image to file. Can also save multiple images.
+    Args:
+        img: image (or list of images) in matrix form
+        path: path (or list of paths) to file
+    """
+    # handle singles or stacks
+    if isinstance(img, list):
+        if not isinstance(path, list):
+            path = [path]
+        if not len(img) == len(path):
+            raise ValueError('if images are in stack, so should be paths')
+    else:
+        img = [img]
+        path = [path]
+
+    # save it
+    for idx, image in enumerate(img):
+        im = deepcopy(image)
+        print 'saving', path[idx]
+        if im.dtype == np.dtype('uint8'):
+            matrix2img(im).save(path[idx])
+        elif im.dtype == np.dtype('float64'):
+            matrix2img(im).save(path[idx])
+        else:
+            raise ValueError("show: unsupported image type: found {0}".format(
+                             im.dtype))
